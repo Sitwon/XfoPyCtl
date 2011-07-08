25e2d8ae35bedd30666811ad0e166ad382452add
Ben Mendis <dragonwisard@gmail.com>
2011-07-08 14:38:33 -0400

    Fixing a pattern in this script

--- preprocess.sh
+++ preprocess.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cat xfoifc_c.h | grep '^XFOINTERFACE_API' | grep -v 'wchar' | sed 's/XFOINTERFACE_API //' | sed 's/const //' | sed 's/xfo_//' > prototypes
+cat xfoifc_c.h | grep '^XFOINTERFACE_API' | grep -v 'wchar' | sed 's/XFOINTERFACE_API //' | sed 's/const //g' | sed 's/xfo_//' > prototypes
